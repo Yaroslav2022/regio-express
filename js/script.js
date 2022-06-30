c9b752e1c549932114a68d6d86b198197ed918e1
Yaroslav <yaribun@mail.ru>
2022-06-30 14:55:18 +0200

    Finish

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.slider__news').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+    });
+});
+
+$(document).ready(function() {
+    $('.about__right_slider').slick({
+        arrows: true,
+        dots: true,
+
+    });
+});
